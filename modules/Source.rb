4249e1ee07792614681f7860cd81bafe771d62b8
Kyle Jonhson <kyle@vacantminded.com>
2010-05-21 04:17:30 -0500

    Oops

--- modules/Source.rb
+++ modules/Source.rb
@@ -1,4 +1,4 @@
-class Source.rb
+class Source
 
   def cmd_source(message)
     reply(message, "This software is licensed under the GNU Affero General Public License. You may acquire the source code at http://github.com/kabaka/Terminus-Bot or view the full license at http://www.gnu.org/licenses/agpl-3.0.html")
