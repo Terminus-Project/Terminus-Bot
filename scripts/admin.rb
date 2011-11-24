33eaf851038e23b96673c0218c1cdfd8d9f22ff3
Kyle Jonhson <kyle@vacantminded.com>
2011-11-24 14:11:26 +0000

    Add nick command to admin script (currently doesn't check for 433 or existing nicks!)

--- scripts/admin.rb
+++ scripts/admin.rb
@@ -23,12 +23,18 @@ def initialize
 
   register_command("eval",    :cmd_eval,     1, 10,  "Run raw Ruby code.")
   register_command("quit",    :cmd_quit,     1, 10,  "Kill the bot.")
+
   register_command("rehash",  :cmd_rehash,   0,  8,  "Reload the configuration file.")
+  register_command("nick",    :cmd_nick,     1,  7,  "Change the bot's nick for this connection.")
+
   register_command("includes",:cmd_includes, 0,  9,  "Reload core files with stopping the bot. Warning: may produce undefined behavior.")
   register_command("reload",  :cmd_reload,   1,  9,  "Reload the named script.")
   register_command("unload",  :cmd_unload,   1,  9,  "Unload the named script.")
   register_command("load",    :cmd_load,     1,  9,  "Load the named script.")
+
   register_command("raw",     :cmd_raw,      1,  9,  "Send raw text over the IRC connection.")
+
+
 end
 
 def die
@@ -49,6 +55,13 @@ def cmd_rehash(msg, params)
   msg.reply("Done reloading configuration.")
 
   $bot.start_connections
+end
+
+def cmd_nick(msg, params)
+  msg.connection.nick = params[0]
+  msg.raw("NICK #{params[0]}")
+
+  msg.reply("Nick changed to #{params[0]}")
 end
 
 def cmd_includes(msg, params)
