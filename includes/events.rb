49cf8330ba8b52f05b41e24197b14fddd471bd50
Kyle Jonhson <kyle@vacantminded.com>
2011-11-24 08:44:35 +0000

    Events now extends Hash.

--- includes/events.rb
+++ includes/events.rb
@@ -21,41 +21,40 @@ module Terminus_Bot
 
   Event = Struct.new(:name, :func, :owner)
 
-  class Events
+  class Events < Hash
 
     # Create a hash table for event storage.
     def initialize
-      @events = Hash.new
     end
 
     # Create a new event. The key in the hash table is the event name
     # which is used to run the event. The value is an array which will store
     # the multiple events that run are run when the event name is called.
     def create(owner, name, func)
-      unless @events.has_key? name
-        @events[name] = Array.new
+      unless self.has_key? name
+        self[name] = Array.new
       end
 
       $log.debug("events.create") { "Created event #{name}" }
-      @events[name] << Event.new(name, func, owner)
+      self[name] << Event.new(name, func, owner)
     end
 
     # Run all the events with the given name.
     # TODO: Find out why I used *args and change it if it doesn't need to be
     #       that way.
     def run(name, *args)
-      return unless @events.has_key? name
+      return unless self.has_key? name
 
       $log.debug("events.run") { name }
 
-      @events[name].each do |event|
+      self[name].each do |event|
         event.owner.send(event.func, args[0])
       end
     end
 
     # Delete all the events owned by the given class.
     def delete_events_for(owner)
-      @events.each do |n, a|
+      self.each do |n, a|
         a.delete_if {|e| e.owner == owner}
       end
     end
