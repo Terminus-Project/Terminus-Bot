63b171790b01bfaddd53f82497013b88b76e8bee
Kyle Jonhson <kyle@vacantminded.com>
2011-11-26 08:18:01 +0000

    Ping script now measures CTCP PING response time.

--- scripts/ping.rb
+++ scripts/ping.rb
@@ -19,16 +19,55 @@
 
 
 def initialize
-  register_script("Simple ping/pong.")
+  register_script("Bot to user response time checker.")
 
-  register_command("ping", :pong,  0,  0, "Send a pong.")
+  register_command("ping", :cmd_ping,  0,  0, "Measure the time it takes for the bot to receive a reply to a CTCP PING from your client.")
+
+  register_event("NOTICE", :on_notice)
+
+  @pending = Hash.new
 end
 
 def die
   unregister_script
   unregister_commands
+  unregister_events
 end
 
-def pong(msg, params)
-  msg.reply("Pong!")
+def on_notice(msg)
+  unless @pending.has_key? msg.connection.name
+    return
+  end
+
+  unless @pending[msg.connection.name][msg.nick]
+    return
+  end
+
+  if msg.text == "\01PING\01"
+    time = Time.now.to_f - @pending[msg.connection.name][msg.nick]
+    @pending[msg.connection.name].delete(msg.nick)
+
+    msg.reply("Your ping time: #{sprintf("%2.4f", time)} seconds")
+  end
+end
+
+def cmd_ping(msg, params)
+  unless @pending.has_key? msg.connection.name
+    @pending[msg.connection.name] = Hash.new
+  end
+  
+
+  if @pending[msg.connection.name].has_key? msg.nick
+    if Time.now.to_f - @pending[msg.connection.name][msg.nick] > 30
+      @pending[msg.connection.name].delete(msg.nick)
+    else
+      msg.reply("I'm still waiting on a reply to the last ping I sent you. (It will expire 30 seconds after it was sent.)")
+      return
+    end
+  end
+
+  msg.raw("PRIVMSG #{msg.nick} :\01PING\01")
+
+  # Get a fresh time for slightly increased accuracy.
+  @pending[msg.connection.name][msg.nick] = Time.now.to_f
 end
