dfa333b6cdb64ecd726f645016beb3167182e338
Kyle Jonhson <kyle@vacantminded.com>
2011-11-28 21:21:48 +0000

    Add lograw script. Very elementary, but it's been useful.

---
+++ scripts/lograw.rb
@@ -0,0 +1,36 @@
+
+#
+# Terminus-Bot: An IRC bot to solve all of the problems with IRC bots.
+# Copyright (C) 2011 Terminus-Bot Development Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+def initialize
+  register_script("Dump raw messages to debug log.")
+
+  register_event(:raw, :on_raw)
+end
+
+def die
+  unregister_script
+  unregister_events
+end
+
+def on_raw(msg)
+  $log.debug("lograw.on_raw") { sprintf "O: %s  D: %s  TXT: %s  TYP: %s  NUH: %s!%s@%s  C: %s",
+    msg.origin, msg.destination, msg.text, msg.type, msg.nick, msg.user, msg.host,
+    msg.connection.name }
+end
