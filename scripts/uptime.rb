f223c8dbdc852caf48227b7ba56775fa85230482
Alex Iadicicco <alex@theta4.com>
2012-02-26 04:57:18 +0800

    Uptime now reports duration

--- scripts/uptime.rb
+++ scripts/uptime.rb
@@ -24,7 +24,39 @@ def initialize
   register_command("uptime", :cmd_uptime,  0,  0, "Show how long the bot has been active.")
 end
 
+# I'm probably reinventing the wheel here, but Google returned nothing
+# dur is expected to be seconds
+def time_ago_in_words(dur)
+  dur = dur.to_i
+  t = []
+  # this loop converts seconds into hours, minutes, seconds, stored in t
+  [60, 60, 24].each do |n|
+    dur, q = dur.divmod(n)
+    t << q
+  end
+  t << dur
+
+  # assemble only nonzero pieces
+  # this loop also takes care of pluralization
+  pieces = [[t[3], " day", "s"], [t[2], " hour", "s"], [t[1], " minute", "s"], [t[0], " second", "s"]]
+  pieces.map! do |piece|
+    unless piece[0] == 0
+      s = piece[0] == 1? "" : piece[2] 
+      piece[0].to_s + piece[1] + s
+    end
+  end
+  pieces.compact!
+
+  # tack 'and' onto the last piece unless there's only one piece
+  pieces[-1] = "and " + pieces[-1] unless pieces.length <= 1
+
+  # omit the comma if only two pieces
+  return pieces.join(", ") + " ago" unless pieces.length == 2
+  return pieces.join(" ") + " ago"
+end
+
 def cmd_uptime(msg, params)
-  # TODO: Clean this up. Also, show duration since started.
-  msg.reply("I was started at #{File.ctime(PID_FILE)}. \02In:\02 #{$bot.lines_in} lines (#{sprintf("%.4f", $bot.bytes_in / 1024.0)} KiB) \02Out:\02 #{$bot.lines_out} lines (#{sprintf("%.4f", $bot.bytes_out / 1024.0)} KiB)")
+  # TODO: Clean this up
+  since = time_ago_in_words(Time.new - File.ctime(PID_FILE))
+  msg.reply("I was started #{since}. \02In:\02 #{$bot.lines_in} lines (#{sprintf("%.4f", $bot.bytes_in / 1024.0)} KiB) \02Out:\02 #{$bot.lines_out} lines (#{sprintf("%.4f", $bot.bytes_out / 1024.0)} KiB)")
 end
