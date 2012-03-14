05b7c95a48e591c9414eb0d6e44fafa1fcf720ca
Kyle Jonhson <kyle@vacantminded.com>
2012-03-14 14:51:05 +0000

    Change IdleRPG script config format. Added player info fetching via HTTP/XML.

--- scripts/idlerpg.rb
+++ scripts/idlerpg.rb
@@ -17,36 +17,86 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 # 
 
+require "uri"
+require 'net/http'
+require 'rexml/document'
+require 'htmlentities'
+
 def initialize
   register_script("Play IdleRPG.")
 
-  register_command("idlerpg", :cmd_idlerpg, 0, 5, "Get information about the IdleRPG configuration for this network")
+  register_command("idlerpg", :cmd_idlerpg, 0, 0, "Get information about this network's IdleRPG. Parameters: [player]")
 
   register_event("JOIN", :on_join)
 end
 
 def cmd_idlerpg(msg, params)
-  idleinfo = get_config(msg.connection.name)
+  config = get_config(msg.connection.name)
 
-  if idleinfo == nil
-    msg.reply("I am not configured to play IdleRPG on this network.")
+  if config == nil
+    msg.reply("I am not configured for this network's IdleRPG.")
     return
   end
 
-  bot, chan, login = idleinfo.split(/,/, 3)
-  msg.reply("\02IdleRPG bot:\02 #{bot} \02Channel:\02 #{chan}. I will begin playing when I have joined the channel", false)
+  if params.empty?
+
+    unless config.has_hey? "login_command"
+      msg.reply("I am not configured to play IdleRPG on this network.")
+      return
+    end
+
+    msg.reply("\02IdleRPG Bot:\02 #{config["nick"]} \02Channel:\02 #{config["channel"]}. I will begin playing when I have joined the channel")
+  else
+
+    unless config.has_key? "xml_url"
+      msg.reply("I don't know where to get player info on this network.")
+      return
+    end
+
+    info = get_player_info(params.shift, config)
+
+    if info == nil
+      msg.reply("Player info not found.")
+      return
+    end
+
+    msg.reply(info)
+  end
+end
+
+def get_player_info(player, config)
+  url = "#{config["xml_url"]}#{URI.escape(player)}"
+
+  body = Net::HTTP.get URI.parse(url)
+  root = (REXML::Document.new(body)).root
+
+  return nil if root == nil
+  
+  level = root.elements["//level"].text
+  ttl = root.elements["//ttl"].text.to_i
+  idled = root.elements["//totalidled"].text.to_i
+  klass = root.elements["//class"].text
+
+  ttl = Time.at(Time.now.to_i + ttl).to_duration_s
+  idled = Time.at(Time.now.to_i + idled).to_duration_s
+
+  "\02Level:\02 #{level} \02Class\02: #{klass} \02Time to Level:\02 #{ttl} \02Time Idled:\02 #{idled}"
 end
 
 def on_join(msg)
+
+  # TODO: Log in if the IdleRPG bot is rejoining. Most bots will log users back
+  # in based on hostname, but that's difficult to determine, so we should try to
+  # log back in regardless.
+
   return unless msg.me?
   
-  idleinfo = get_config(msg.connection.name, nil)
-  return if idleinfo == nil
+  config = get_config(msg.connection.name)
 
-  bot, chan, login = idleinfo.split(/,/, 3)
+  return if config == nil
+  return unless config["channel"].downcase == msg.destination.downcase
+  return unless config.has_hey? "login_command" and config.has_key? "nick"
 
-  return unless chan.downcase == msg.destination.downcase
-
-  msg.send_privmsg(bot, login)
+  msg.send_privmsg(config["nick"], config["login_command"])
 end
 
