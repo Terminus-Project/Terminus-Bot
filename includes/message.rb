f784c09166fbd26abe0139585d4eda79c897e271
Kyle Jonhson <kyle@vacantminded.com>
2012-03-17 02:36:23 +0000

    Fix message.rb parsing for IPv6 users.

--- includes/message.rb
+++ includes/message.rb
@@ -43,7 +43,7 @@ class Message
       @type = arr[0]
       @destination = arr[1]
       
-      @text = (str =~ /\A[^:]+:(.+)\Z/ ? $1 : "")
+      @text = (str =~ /\A.+\s:(.+)\Z/ ? $1 : "")
 
     else
 
@@ -62,7 +62,7 @@ class Message
 
         # Grab the text portion, as in
         # :origin PRIVMSG #dest :THIS TEXT
-        @text = (str =~ /\A:[^:]+:(.+)\Z/ ? $1 : "")
+        @text = (str =~ /\A:.+\s:(.+)\Z/ ? $1 : "")
 
       else
         # Server PINGs. Not much else.
