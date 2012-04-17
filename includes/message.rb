Terminus-Project/Terminus-Bot

commit 0ddf12840ff9406c757d3b3f21e655e7e109646e

Change silent setting to use a block rather than some wonky string format. ** THIS REQUIRES CONFIG CHANGES. **

includes/message.rb
@@ -113,23 +113,20 @@ class Message
   # Return true if this channel is listed in the silent setting.
   def silent?
     return false if self.private?
+
     silenced = $bot.config['core']['silent']
 
     return false if silenced == nil
-
-    silenced = silenced.split(" ")
-    
     return false if silenced.empty?
 
-    silenced.each do |s|
-      s = s.split(":")
+    silenced.each_pair do |connection, channels|
+      next unless connection == @connection.name
+      next if channels.empty?
 
-      next if s.length != 2
-
-      return true if s[0] == @connection.name and s[1] == @destination
+      return true if channels.include? @destination
     end
 
-    return false
+    false
   end
 
   # Attempt to truncate messages in such a way that the maximum

terminus-bot.conf.dist
@@ -67,8 +67,10 @@ core = {
   # Channels in which the bot should never speak. This is useful if you want the
   # bot to log chat for a channel but not actually contribute or do anything. Note
   # that it is possible to write scripts which ignore this setting!
-  # format: server:#foo server:#bar server2:#baz
-  #silent =
+  # format: server = #channel #channel2 #channel3
+  #silent = {
+  #  freenode = #botters
+  #}
 
   # OPTIONAL
   # If your bot is running on a machine with multiple IP addresses, you should use

commit fc63aaa2d19aa72701a16b36ba0a332c6a326eeb

Update to fix a bug introduced by 0ddf128.

includes/message.rb
@@ -135,6 +135,8 @@ class Message
       next unless connection == @connection.name
       next if channels.empty?
 
+      channels = channels.split
+
       return true if channels.include? @destination
     end
 