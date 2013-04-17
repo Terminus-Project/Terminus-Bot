3c6212f56b27c57be5b7d26360cd296bc1dc23b0
Kyle Jonhson <kyle@vacantminded.com>
2013-04-17 06:12:40 +0000

    scripts/karma: new karma tracker (nick++/nick--)

---
+++ scripts/karma.rb
@@ -0,0 +1,67 @@
+#
+# Terminus-Bot: An IRC bot to solve all of the problems with IRC bots.
+#
+# Copyright (C) 2013 Kyle Johnson <kyle@vacantminded.com>, Alex Iadicicco
+# (http://terminus-bot.net/)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+register 'Track user karma.'
+
+
+command 'karma', 'Check karma or yourself or the specified nick.' do
+  target = @params.empty? ? @msg.nick : @params.first.strip
+
+  karma = get_data(@connection.name, Hash.new(0))[target.upcase]
+
+  unless karma
+    raise "#{target} has no karma."
+  end
+
+  reply "#{target}'s karma is #{karma}"
+end
+
+event :PRIVMSG do
+  next if query?
+
+  match = @msg.text.match(/^(?<target>[^ ]+)(?<change>\+\+|--)(\s|$)/)
+
+  next unless match
+
+  target = match[:target].upcase
+
+  unless @connection.channels[@msg.destination_canon].users.has_key? target
+    $log.debug('karma.privmsg') { "Skipping nonexistent target #{target}" }
+    next
+  end
+
+  karma = get_data @connection.name, Hash.new(0)
+
+  if match[:change] == "++"
+    karma[target] += 1
+  else
+    karma[target] -= 1
+  end
+
+  $log.debug('karma.privmsg') { "#{target} karma: #{karma[target]}" }
+
+  store_data @connection.name, karma
+end
+
