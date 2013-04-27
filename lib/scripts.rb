2621e1972db5ecbc49d42f35d4d19ea77d7fa315
Kyle Jonhson <kyle@vacantminded.com>
2013-04-27 15:26:10 +0000

    doc: add Script class docs

--- lib/scripts.rb
+++ lib/scripts.rb
@@ -34,10 +34,15 @@ module Bot
 
     # TODO: Rework this whole file. Stop this stupid string juggling.
 
+
+    # Initialize a new ScriptManager object.
+    #
+    # @raise if scripts directory does not exist
     def initialize
       @scripts, @script_info = {}, []
 
       unless Dir.exists? "scripts"
+        # XXX - raise a better exception here
         raise "Scripts directory does not exist."
       end
     end
@@ -61,13 +66,17 @@ module Bot
       end
     end
 
-    # Run the die functions on all scripts.
+    # Run the `die` functions on all scripts that respond to it.
     def die
+      # XXX
       @scripts.each_value {|s| s.die if s.respond_to? "die"}
     end
 
     # Load the given script by file name. The relative path should be included.
     # Scripts are expected to be in the scripts dir.
+    # @param filename [String] relative path to script
+    # @raise if script is already loaded
+    # @raise if an error occurs while evaluating the script
     def load_file filename
       unless File.exists? filename
         raise "File #{filename} does not exist."
@@ -114,7 +123,9 @@ module Bot
     end
 
     # Unload and then load a script. The name given is the script's short name
-    # (script/short_name.rb).
+    # (script/**short_name**.rb).
+    # @param name [String] script short name
+    # @raise if script does not exist
     def reload name
       raise "Cannot reload: No such script #{name}" unless @scripts.has_key? name
 
@@ -137,7 +148,9 @@ module Bot
     end
 
     # Unload a script. The name given is the script's short name
-    # (scripts/short_name.rb).
+    # (scripts/**short_name**.rb).
+    # @param name [String] script short name
+    # @raise if script does not exist
     def unload name
       raise "Cannot unload: No such script #{name}" unless @scripts.has_key? name
 
@@ -153,6 +166,10 @@ module Bot
       @scripts.delete name
     end
 
+    # Register a new script with the script info list, flags, and any other
+    # data structures that need to know about new scripts.
+    #
+    # @see Script_Info#initialize
     def register_script *args
       $log.debug("ScriptManager.register_script") { "Registering script: #{args.to_s}" }
 
@@ -164,6 +181,8 @@ module Bot
       @script_info.sort_by! {|s| s.name}
     end
 
+    # Delete a script from the script info list.
+    # @param name [String] script short name
     def unregister_script name
       $log.debug("ScriptManager.register_script") { "Unregistering script: #{name}" }
       @script_info.delete_if {|s| s.name == name}
@@ -172,34 +191,93 @@ module Bot
 
   class Script
 
-    # Pass along some register commands with self or our class name attached
-    # as needed. This just makes code in the scripts a little shorter.
-
+    # Enforce module requirements in scripts. Should be the first code in
+    # scripts that require any modules.
+    # 
+    # @example
+    #     need_module! 'http_client'
+    #
+    # @param name [String] name of module that is required
+    # @raise if the required module is not loaded
     def need_module! name
       unless Bot.const_defined? "MODULE_LOADED_#{name.upcase}"
         raise "#{my_short_name} requires the #{name} module"
       end
     end
 
+    # Create a new event handler. Multiple event names can be specified to
+    # handle multiple events with one block.
+    # 
+    # @example Handle one type of event
+    #     event :PRIVMSG do
+    #       # ...
+    #     end
+    #
+    #     event :NOTICE do
+    #       # ...
+    #     end
+    #
+    # @example Handle multiple events with one block
+    #     event :PART, :KICK, :QUIT do
+    #       # ...
+    #     end
+    #
+    # @param names [Symbol]
     def event *names, &blk
       names.each do |name|
         Bot::Events.create name, self, nil, &blk
       end
     end
 
+    # Create a new command handler.
+    #
+    # @example Command without help
+    #     command 'hello' do
+    #       reply 'hello!'
+    #     end
+    #
+    # @example Command with help
+    #     command 'hello', 'Say hello to the bot.' do
+    #       reply 'hello!'
+    #     end
+    #
+    # @param cmd [String] one-word command trigger
+    # @param help [String] helpful command description for bot users
     def command cmd, help = "", &blk
       Bot::Commands.create self, cmd, help, &blk
     end
 
-
+    # Regiser the script with the bot. This is not mandatory, but is
+    # recommended so the script list includes data about your script.
+    #
+    # @example
+    #     register 'an exmaple script'
+    #
+    # @param args [String] helpful description of the script
     def register *args
       Bot::Scripts.register_script my_short_name, *args
     end
 
+    # Evaluate a block of code at the class level. Ideal for adding helper
+    # functions to your script since they cannot be declared at the top
+    # level of the script.
+    #
+    # @example
+    #     helpers do
+    #       def foo
+    #         # ...
+    #       end
+    #
+    #       def bar arg
+    #         # ...
+    #       end
+    #     end
     def helpers &blk
       @helpers = blk
     end
 
+    # Get the helpers block for this script. Used by the command dispatcher.
+    # @todo move this somewhere better
     def get_helpers
       @helpers
     end
@@ -207,14 +285,21 @@ module Bot
 
     # Shortcuts for unregister stuff. Makes teardown easier in die methods.
 
+
+    # Delete all commands owned by this script. Used by {ScriptManager} when
+    # unloading the script.
     def unregister_commands
       Bot::Commands.delete_for self
     end
 
+    # Delete all event handlers owned by this script. Used by {ScriptManager}
+    # when unloading the script.
     def unregister_events
       Bot::Events.delete_for self
     end
 
+    # Delete registered info for this script. Used by {ScriptManager} when
+    # unloading the script.
     def unregister_script
       Bot::Scripts.unregister_script my_short_name
     end
@@ -222,21 +307,44 @@ module Bot
 
     # Dunno if these should be functions or variables. Feel free to change.
 
-
+    
+    # Get the script's long name, such as `Script_help`.
+    # @return [String] script name
     def my_name 
       @my_name ||= self.class.name.split("::").last
     end
 
+    # Get the script's short name, such as `help`.
+    # @return [String] short script name
     def my_short_name 
       @my_short_name ||= self.class.name.split("_").last
     end
 
 
-    # Get config data for this script, if it exists. The section name
-    # in the config is the script's short name. Configuration in this
-    # version of Terminus-Bot is read-only, unlike the YAML-based config
-    # in the previous version. If you want to store data, you want to use
-    # the database. See functions below for that!
+    # Get config data for this script, if it exists.
+    #
+    # The section name in the config is the script's short name.
+    #
+    # @example Configuration file entry for "example" script
+    #     example = {
+    #       foo = bar
+    #       baz = false
+    #     }
+    #
+    # @example Getting configuration values for "example" script
+    #     get_config :foo       #=> "bar"
+    #     get_config :baz, true #=> false
+    #     get_config :qux, 5    #=> 5
+    #
+    # **Note: Configuration in this version of Terminus-Bot is read-only,
+    # unlike the YAML-based config in the previous version. If you want to
+    # store data, you want to use the database.**
+    #
+    # @param key [Symbol] name of setting to get
+    # @param default [Object] value to return if setting does not exist
+    #
+    # @return [String, Boolean, Integer, Object] setting value, or `default` if
+    #   the setting does not exist
     def get_config key, default = nil
       name_key = my_short_name.to_sym
 
@@ -249,22 +357,39 @@ module Bot
       default
     end
 
-    # Check if the database has a Hash table for this plugin. If not,
-    # create an empty one.
+    # If the script's database does not yet exist, create it.
+    #
+    # *Should not be called by scripts.*
+    #
+    # @todo rename, refactor callers
+    # @return [Hash] script database
     def init_data
       Bot::DB[my_name] ||= {}
     end
 
-    # Get the value stored for the given key in the database for this
-    # script. The optional default value is what is returned if no value
-    # exists for the given key.
+    # Get the value stored for the given key in the database for this script.
+    # The optional default value is what is returned if no value exists for the
+    # given key.
+    #
+    # @example Look up database data
+    #     # Existing value, symbol key
+    #     get_data :foo         #=> 'bar'
+    #     # Nonexistent value, string key
+    #     get_data 'bar'        #=> nil
+    #     # Nonexistent value, string key
+    #     get_data 'baz', 'qux' #=> "qux"
+    #
+    # @param key [Object] database entry key
+    # @param default [Object] value to return if database entry does not exist
+    # @return [Object] database entry, or `default` if none exists
     def get_data key, default = nil
       init_data
 
       Bot::DB[my_name][key] or default
     end
 
-    # Get all of the data for this script.
+    # Get the entire database for this script.
+    # @return [Hash] script database
     def get_all_data
       init_data
 
@@ -272,6 +397,19 @@ module Bot
     end
 
     # Store the given value in the database if one isn't already set.
+    # 
+    # @example Setting default values.
+    #     default_data :messages_seen, 0
+    #
+    #     # count PRIVMSG events
+    #     event :PRIVMSG do
+    #       seen = get_data :messages_seen #=> 0
+    #       seen += 1                      #=> 1
+    #       store_data :messages_seen, :seen
+    #     end
+    #
+    # @param key [Object] database item keye
+    # @param value [Object] value to store if none is set
     def default_data key, value
       init_data
 
@@ -279,6 +417,14 @@ module Bot
     end
 
     # Store a value in the database under the given key.
+    #
+    # @example Storing values in the database
+    #     event :PRIVMSG do
+    #       store_data :last_message, @msg
+    #     end
+    #
+    # @param key [Object] database item key
+    # @param value [Object] value to store in database
     def store_data key, value
       init_data
 
@@ -286,12 +432,15 @@ module Bot
     end
 
     # Delete data under the given key, if it exists.
+    # @param key [Object] database key to delete
     def delete_data key
       init_data
 
       Bot::DB[my_name].delete key
     end
 
+    # @see Script#my_short_name
+    # @return [String] script short name
     def to_str
       my_short_name
     end
