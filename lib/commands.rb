a192cc305f3813547f0942896e8295ece2c04e79
Kyle Jonhson <kyle@vacantminded.com>
2013-04-27 16:02:42 +0000

    lib/commands: for commands prefixed with bot nick, allow any trailing characters

--- lib/commands.rb
+++ lib/commands.rb
@@ -41,7 +41,7 @@ module Bot
     def self.on_privmsg msg
       prefix = Regexp.escape Bot::Conf[:core][:prefix]
 
-      match = msg.text.match(/^(?<prefix>#{prefix}|#{msg.connection.nick}.? )?(?<command>[^ ]+)( (?<params>.+))?/i)
+      match = msg.text.match(/^(?<prefix>#{prefix}|#{msg.connection.nick}[^ ]* )?(?<command>[^ ]+)( (?<params>.+))?/i)
 
       return unless msg.query? or match[:prefix]
 
