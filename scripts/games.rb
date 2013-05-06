df442666e7b834aef558b9e147b8fa0584fa90f8
Kyle Jonhson <kyle@vacantminded.com>
2013-05-06 18:46:49 +0000

    scripts/games: fix spacing in copyright

--- scripts/games.rb
+++ scripts/games.rb
@@ -2,8 +2,7 @@
 # Terminus-Bot: An IRC bot to solve all of the problems with IRC bots.
 #
 # Copyright (C) 2010-2013 Kyle Johnson <kyle@vacantminded.com>, Alex Iadicicco,
-# David Farrell <shokku.ra@gmail.com>
-# (http://terminus-bot.net/)
+# David Farrell <shokku.ra@gmail.com> (http://terminus-bot.net/)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
