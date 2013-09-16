3d4f79c67a1df2f736accb5d27fb51b989f6fb89
Shockk <shokku.ra@gmail.com>
2013-09-16 20:24:22 +0000

    scripts/base: fix name in copyright notice

--- scripts/base.rb
+++ scripts/base.rb
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2010-2013 Kyle Johnson <kyle@vacantminded.com>, Alex Iadicicco,
 # Marshall Fowler (http://terminus-bot.net/), Jade Rain,
-# shockkolate <shokku.ra@gmail.com>
+# Shockk <shokku.ra@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
