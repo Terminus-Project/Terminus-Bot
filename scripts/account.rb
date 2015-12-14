53eeec1c6e88c3930305094aaaad56063ae20eee
Kyle Johnson <kyle@vacantminded.com>
2015-12-14 06:19:06 +0000

    scripts/account: add missing comma (oops)

--- scripts/account.rb
+++ scripts/account.rb
@@ -138,7 +138,7 @@ command 'level', 'Change a user\'s account level. Parameters: username level' do
 
   # if they are logged in, update the live data too
 
-  Connections.each do |_ conn|
+  Connections.each do |_, conn|
     conn.users.each do |_, user|
       if user.account == @params[0]
         user.level = level
